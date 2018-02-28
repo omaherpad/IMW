484ae63d798db081f292c12723185ee7ae451059
omaherpad <32093510+omaherpad@users.noreply.github.com>
2018-02-28 20:38:36 +0000

    Introducción_PHP

---
+++ ejercicio1.php
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <?php
+      $imagen = mt_rand (1,2);
+      echo("<img src='$imagen.png'>");
+    ?>
+  </body>
+</html>)
+
---
+++ ejercicio3.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+  $green = mt_rand(0, 255);
+  $red = mt_rand(0, 255);
+  $blue = mt_rand(0, 255);
+
+  echo("<body style = 'background-color: rgb($red, $green, $blue)'; </body>");
+?>
+</body>
+
+</html>
+
